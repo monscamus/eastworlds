1642fce3dce5795fedaba2a747527cb3627d5902
coppergames <alex.weinle@gmail.com>
2024-05-02 19:54:10 +0100

    green test

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <main
           className={`flex min-h-screen flex-col place-items-center ${oswald.variable} font-sans`}
         >
-          <div className="max-w-2xl lg:text-xl font-light px-8 bg-blue-800/60 text-black">{children}</div>
+          <div className="max-w-2xl lg:text-xl font-light px-8 bg-green-900/60 text-black">{children}</div>
         </main>
       </body>
     </html>
