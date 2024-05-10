11443107ce028bc5c6d92a0cdad3fa318061874f
coppergames <alex.weinle@gmail.com>
2024-05-10 10:24:23 +0100

    text

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col place-items-center gap-4 pt-20 text-white">
       <h1 className={`sam-title text-5xl ${staat.className}`}>EASTWORLDS</h1>
       <p className={`pt-12 mt-14 ${staat.className}`}>
-      Eastworlds is the east of England world-building forum. We welcome people from across all genres
-      including science fiction, fantasy, history, horror and many others. If you don&apos;t live in the east or even in the 
-      UK still get in touch. Our events are set in the East but we love to discuss world-building with as many and as diverse 
-      a group as we possibly can.
+      <b>Hey, are you a fan or maker of imagined worlds in books, games or movies? Welcome to Eastworlds!</b> For folks from across all genres
+      including science fiction, fantasy, history, horror and many others. We&apos;re based in the east of England (hence Eastworlds) or even in the 
+      UK, but we love to discuss world-building with as many and as diverse a bunch as we possibly can.
       </p>
 
       <p className={`pt-6 ${staat.className}`}>
-        Eastworlds acts as an ecumenical council for groups like the BSFA, BFS, Mythohistorical Society, Anglia Ruskin University, UEA, Cambridge Univeristy, 
-        The Tolkien Society, University of Suffolk and many others. We promote these groups&apos; world=building events and talk about what they can learn from each other.
+        Eastworlds acts as an ecumenical council for groups like the BSFA, BFS, Mythohistorical Society, Anglia
+        Ruskin University, UEA, Cambridge Univeristy, 
+        The Tolkien Society, University of Suffolk and many others. We promote these groups&apos; 
+        world-building events and talk about what they can learn from each other. 
       </p>
 
       <p className={`pt-6 ${staat.className}`}>
